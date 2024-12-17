e33fb23bc28f00833f3da52666faab34e3d7beaa
Larissa Oliveira <lah.rio123@gmail.com>
2024-12-17 20:26:36 -0300

    Initial commit

---
+++ src/main/java/com/larissa/pock_spring_app/PockSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.larissa.pock_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PockSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PockSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larissa/pock_spring_app/PockSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissa.pock_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PockSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
